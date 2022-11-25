e8e132ac5f43344a9b30bd621c1e480f57873a06
ahmos <kamaboko_0228@icloud.com>
2022-11-25 23:17:40 +0900

    Hello_Worldとプロジェクトの作成を行いました

---
+++ hello_cargo/target/debug/deps/hello_cargo-1690ef88d930c8b3.d
@@ -0,0 +1,5 @@
+/Users/user/rust_practice/hello_cargo/target/debug/deps/hello_cargo-1690ef88d930c8b3: src/main.rs
+
+/Users/user/rust_practice/hello_cargo/target/debug/deps/hello_cargo-1690ef88d930c8b3.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/user/rust_practice/hello_cargo/target/debug/hello_cargo: /Users/user/rust_practice/hello_cargo/src/main.rs
